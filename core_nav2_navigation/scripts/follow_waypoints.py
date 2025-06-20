bdfe57d3fdd83df6681a8050a59ce0dc94fc22e1
Max van IJsseldijk <m.j.c.b.v.ijsseldijk@student.tue.nl>
2025-06-20 07:28:07 +0000

    Rollback task QOS changes due to issues receiving new tasks

--- core_nav2_navigation/scripts/follow_waypoints.py
+++ core_nav2_navigation/scripts/follow_waypoints.py
@@ -12,18 +12,17 @@ def main():
 
     # Wait for navigation to fully activate
     navigator.waitUntilNav2Active(localizer=f"{namespace}/planner_server")
-    first_call = True
     while rclpy.ok():
-        if first_call:
-            i = 0
-            navigator.previous_task_number = navigator.task_number
-            task_points = navigator.update_goal()
-            first_call = False
+        i = 0
+        navigator.previous_task_number = navigator.task_number
+        task_points = navigator.update_goal()
         navigator.goThroughPoses(task_points)
 
         while not navigator.isTaskComplete():
             if navigator.new_task_requested():
-                task_points = navigator.update_goal()
+                new_points = navigator.update_goal()
+                navigator.goThroughPoses(new_points)
+
             i = i + 1
 
     navigator.destroy_node()
